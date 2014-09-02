943f18e30e845e88936392e7b6e9a1fdd81e7c54
jakecatt <jakecatt@gmail.com>
2014-09-02 17:12:14 -0400

    added logic to controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,10 @@
 class RestaurantsController < ApplicationController
 	def index
 		@user = User.find( params[:user_id] )
-		@restaurants = Restaurant.all
+		if @user.age > 20
+			@restaurants = Restaurant.all
+		else
+			@restaurants = Restaurant.where( :serves_alcohol => false )
+		end
 	end
 end
